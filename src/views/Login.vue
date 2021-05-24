44c25866d74a45ce1a64f03bb0cfc71a563f1b84
Yaroslav <yaroslavvoxon@gmail.com>
2021-05-24 11:54:24 +0300

    Login page finish validate + add switcher on password field

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,25 +1,49 @@
 <template>
   <section class="form-wrap">
     <form class="form" @submit.prevent="submitHandler">
-      <h2>Login</h2>
-      <label class="form__label"
-        >Email
-        <input
-          type="text"
-          placeholder="Example@gmail.com"
-          class="form__field"
-          :class="{ invalid: ($v.email.$dirty && !$v.email.required) ||  ($v.email.$dirty && !$v.email.email)}"
-          v-model.trim="email"
-        />
-      </label>
-      <label class="form__label"
-        >Password
-        <input
-          type="password"
-          class="form__field--password"
-          v-model.trim="password"
-        />
-      </label>
+      <h2 class="form__title">Login</h2>
+      <label class="form__label">Email </label>
+
+      <input
+        type="text"
+        placeholder="Example@gmail.com"
+        class="form__field"
+        :class="{
+          invalid:
+            ($v.email.$dirty && !$v.email.required) ||
+            ($v.email.$dirty && !$v.email.email),
+        }"
+        v-model.trim="email"
+      />
+      <small v-if="$v.email.$dirty && !$v.email.required" class="invalid"
+        >Email required
+      </small>
+      <small v-else-if="$v.email.$dirty && !$v.email.email" class="invalid"
+        >Email not correct
+      </small>
+      <label class="form__label">Password </label>
+
+      <input
+        :type="passwordFieldType"
+        class="form__field--password"
+        :class="{
+          invalid:
+            ($v.password.$dirty && !$v.password.required) ||
+            ($v.password.$dirty && !$v.password.minLength),
+        }"
+        v-model.trim="password"
+      />
+      <span class="place-icon icon-eye" @click="switchVisibility"></span>
+
+      <small v-if="$v.password.$dirty && !$v.password.required" class="invalid"
+        >Password required
+      </small>
+      <small
+        v-else-if="$v.password.$dirty && !$v.password.minLength"
+        class="invalid"
+        >Wrong Password, min {{ $v.password.$params.minLength.min }} symbols.
+        Now password: {{ password.length }}
+      </small>
       <div class="form__reminder">Don’t remember password?</div>
       <v-button type="submit">Continue</v-button>
     </form>
@@ -41,18 +65,27 @@ export default {
   data: () => ({
     email: "",
     password: "",
+    passwordFieldType: "password",
   }),
   validations: {
     email: { email, required },
-    password: { minLength, required },
+    password: { minLength: minLength(8), required },
   },
   methods: {
+    switchVisibility() {
+      this.passwordFieldType =
+        this.passwordFieldType === "password" ? "text" : "password";
+    },
     submitHandler() {
       if (this.$v.$invalid) {
-        this.$v.$touch()
-        console.log('err')
-        return
+        this.$v.$touch();
+        return;
       }
+      const formData = {
+        email: this.email,
+        password: this.password,
+      };
+      console.log(formData);
       this.$router.push("/home");
     },
   },
@@ -73,17 +106,22 @@ export default {
   justify-content: space-between;
   padding: 25px 24px 21px;
   width: 425px;
-  height: 365px;
+  min-height: 365px;
   background: #ffffff;
   box-shadow: 0px 2px 42px rgba(0, 0, 0, 0.111233);
   border-radius: 7px;
 
+  &__title {
+    text-align: center;
+    margin-bottom: 28px;
+  }
   &__label {
     text-transform: uppercase;
     font-size: 12px;
     line-height: 14px;
     letter-spacing: 0.3px;
     display: block;
+    margin-bottom: 5px;
   }
 
   &__field {
@@ -105,10 +143,6 @@ export default {
     box-sizing: border-box;
     border-radius: 5px;
     margin-bottom: 15px;
-    background-image: url("../assets/img/eye.svg");
-    background-repeat: no-repeat;
-    cursor: pointer;
-    background-position: 95% 50%;
   }
 
   &__reminder {
@@ -133,5 +167,25 @@ export default {
     text-decoration: none;
     list-style: none;
   }
+}
+
+.invalid {
+  font-size: 12px;
+  font-weight: 300;
+  color: red;
+}
+.place-icon {
+  position: relative;
+}
+.icon-eye {
+  background-image: url("../assets/img/eye.svg");
+  background-repeat: no-repeat;
+  content: "";
+  width: 24px;
+  height: 24px;
+  position: relative;
+  left: 335px;
+  top: -54px;
+  cursor: pointer;
 }
 </style>
